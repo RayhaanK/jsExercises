8183a8ac6275fc44fde0d4c5065abaf7905748c6
RayhaanK <rayhaankhan331@gmail.com>
2023-05-24 15:44:54 +0200

    finished q1

---
+++ script/code.js
@@ -0,0 +1,45 @@
+
+// --Variable--
+// A variable is a word or container used to store information.
+
+// --Value--
+// A value can vary from being a number, text as well as some functions
+
+// --Data Type--
+// A Data Type is the desctription of the data that is stored in a variable. This can either be a : string, boolean, number, undefined, null, array, object. 
+
+// --Initialization--
+// Initialization is when you store a value in a variable.
+
+// --Whats the difference between let, var, and const--
+// If you use const, it means the variable can never be changed or reassigned.
+// If you use let and var, the variable can be reassigned.
+// The difference between let and var is that var declares the variable globally, you are able to declare the variable again and they do not support block-level scope. With regards to let, let declared the variable locally, it is not able to be redeclared it can only be reassigned, and it does support block-level scope.
+
+// --What is a variable scope?--
+// Variable scope defines the range of a program or code where a variable can be referenced.  It can be global, meaning it is defined everywhere and can also be defined locally.
+
+// --When can you decide when to use a for or while, forEach and do while loop?--
+// for loops are used through a block of code a number of times
+// while loops are used through a block of code while a specified condition is true
+// for/in loops are used through the properties of an object
+// for/of loops are used through the values of an iterable object
+// do while are also loops we use through a block of code while a specified condition is true
+// foreach is used whenever your array is associtive or has gaps
+
+// --Define hoisting?--
+// Hoisting allows us to call functions, variables or classes before they appear in the code. They move it to the top of the scope, prior to the execution of the code.
+
+
+
+// how to display the details of someone's fullName
+let fullName = {
+    firstName : 'Mohamed',
+    middleName : 'Rayhaan',
+    lastName : 'Khan'
+};
+console.log(fullName);
+
+// end of q1
+    
+
